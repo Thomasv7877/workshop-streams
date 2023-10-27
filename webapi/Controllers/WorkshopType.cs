6ad32792ffe14843dc4ae56e6f5a77347fb60023
Thomas Van den Hauwe <tvandenhauwe@gmail.com>
2023-10-27 17:26:44 +0200

    comment unnecessary graph type class

--- webapi/Controllers/WorkshopType.cs
+++ webapi/Controllers/WorkshopType.cs
@@ -1,14 +1,14 @@
-﻿using models.model;
+﻿//using models.model;
 
-namespace API.Controllers
-{
-    public class WorkshopType : ObjectType<Workshop>
-    {
-        protected override void Configure(IObjectTypeDescriptor<Workshop> descriptor)
-        {
-            base.Configure(descriptor);
-            descriptor.Field(r => r.Id).Type<NonNullType<IntType>>();
-            descriptor.Field(r => r.description).Type<NonNullType<StringType>>();
-        }
-    }
-}
+//namespace API.Controllers
+//{
+//    public class WorkshopType : ObjectType<Workshop>
+//    {
+//        protected override void Configure(IObjectTypeDescriptor<Workshop> descriptor)
+//        {
+//            base.Configure(descriptor);
+//            descriptor.Field(r => r.Id).Type<NonNullType<IntType>>();
+//            descriptor.Field(r => r.description).Type<NonNullType<StringType>>();
+//        }
+//    }
+//}
